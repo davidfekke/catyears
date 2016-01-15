fee030708937c118af49865fd6df5c1f64f30b12
David Fekke <david@fekke.com>
2016-01-15 16:08:24 -0500

    Adding badges, gulp and eslint

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # CatYears
 
 [![Build Status](https://travis-ci.org/davidfekke/catyears.svg?branch=master)](https://travis-ci.org/davidfekke/catyears)
+[![NPM version][npm-image]][npm-url] 
+[![Downloads][downloads-image]][npm-url]
+[![Coverage Status](https://coveralls.io/repos/davidfekke/catyears/badge.svg?branch=master&service=github)](https://coveralls.io/github/davidfekke/catyears?branch=master)
+[![npm version](https://badge.fury.io/js/catyears.svg)](https://badge.fury.io/js/catyears)
+[![Node v5.0](http://img.shields.io/badge/node-v5.0-brightgreen.svg)](https://nodejs.org)
+[![Node v4.0](http://img.shields.io/badge/node-v4.0-green.svg)](https://nodejs.org)
+[![Node v0.12](http://img.shields.io/badge/node-v0.12-yellowgreen.svg)](https://nodejs.org)
+[![Node v0.10](http://img.shields.io/badge/node-v0.10-yellow.svg)](https://nodejs.org)
+
+[downloads-image]: https://img.shields.io/npm/dm/catyears.svg
+[npm-url]: https://www.npmjs.com/package/catyears
+[npm-image]: https://img.shields.io/npm/v/catyears.svg
 
 Use this module for calculating how old your cat is in cat years.
+
+[![NPM](https://nodei.co/npm/catyears.png)](https://nodei.co/npm/catyears/)
 
 ## Example
 
--- tests/testcatyears.js
+++ tests/testcatyears.js
@@ -1,11 +1,31 @@
+'use strict';
 
 var catyears = require('../lib/catyears.js');
-var mocha = require('mocha');
 var expect = require('chai').expect;
 
-describe('Test CatYears module', function() {
-	it('Test result of a cat that 10 in actual years', function() {
-		var result = catyears(10);
-		expect(57).to.equal(result);
-	});
+describe('Test CatYears module', function () {
+    it('Test result of a cat that 10 in actual years', function () {
+        var result = catyears(10);
+        expect(57).to.equal(result);
+    });
+
+    it('Test result of a cat that is only 1 in actual years', function () {
+        var result = catyears(1);
+        expect(15).to.equal(result);
+    });
+
+    it('Test result of a cat that is 1.5 years old in actual years', function () {
+        var result = catyears(1.5);
+        expect(20).to.equal(result);
+    });
+
+    it('Test result of a cat that is 2 years old in actual years', function () {
+        var result = catyears(2);
+        expect(25).to.equal(result);
+    });
+
+    it('Test result of a cat that is 3 years old in actual years', function () {
+        var result = catyears(3);
+        expect(29).to.equal(result);
+    });
 });
